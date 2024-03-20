591195d06fe56100d00251b66e73c0f4554e6bfb
querriwill <will.horn@querri.com>
2024-03-19 21:00:01 -0400

    updated webClient

--- webClient/angular/src/environments/environment.ts
+++ webClient/angular/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  region: 'your region',
-  userPoolId: 'your user pool id',
-  userPoolWebClientId: 'your client id',
+  region: 'us-east-1',
+  userPoolId: 'us-east-1_p2pq025hF',
+  userPoolWebClientId: '3mjd89hpt0r9olrko46f5a3e7k',
 };
 
 /*
